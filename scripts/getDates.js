8685c6d456e83f4d32f3e19ff520301bfcaa08b5
jvgar10 <maddogjig@hotmail.com>
2024-01-11 13:21:02 -0600

    fix

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,7 @@
 
 
-// let nLastModif = Date.parse(document.lastModified
+// let nLastModif = Date.parse(document.lastModified);
+console.log(document.lastModified)
+
+// document.getElementById('lastModified')
+document.querySelector('#lastModified').innerHTML = document.lastModified;
