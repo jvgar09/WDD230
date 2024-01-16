c2af955ae3afe67ffa7904d69854cbf1306e9493
jvgar10 <maddogjig@hotmail.com>
2024-01-15 19:41:48 -0600

    dark mode and ham manu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -15,3 +15,18 @@ console.log(document.lastModified)
 document.querySelector('#lastModified').innerHTML = document.lastModified;
 
 
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
