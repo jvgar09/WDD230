5a6e10a617bec3e54ad7de5158fadc8fafbce839
jvgar10 <maddogjig@hotmail.com>
2024-01-15 18:43:59 -0600

    add hammenu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,16 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
 // let nLastModif = Date.parse(document.lastModified);
 console.log(document.lastModified)
 
 // document.getElementById('lastModified')
 document.querySelector('#lastModified').innerHTML = document.lastModified;
+
+
