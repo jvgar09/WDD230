113f5bf6c2b2a60d01e1b46b9dd233202283d47c
jvgar10 <maddogjig@hotmail.com>
2024-01-15 19:15:29 -0600

    ading the ham menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,11 +1,12 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
 });
-
 
 // let nLastModif = Date.parse(document.lastModified);
 console.log(document.lastModified)
