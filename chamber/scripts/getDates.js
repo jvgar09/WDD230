3c308f411b13f3b89fbd95fe99bf837331b9d2dd
jvgar10 <maddogjig@hotmail.com>
2024-01-16 23:16:13 -0600

    add

--- chamber/scripts/getDates.js
+++
@@ -1,5 +0,0 @@
-// let nLastModif = Date.parse(document.lastModified);
-console.log(document.lastModified)
-
-// document.getElementById('lastModified')
-document.querySelector('#lastModified').innerHTML = document.lastModified;
