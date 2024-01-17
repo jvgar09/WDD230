17ab5da3c80f346faa0eb6614e2b0a7d76e89c9a
jvgar10 <maddogjig@hotmail.com>
2024-01-16 22:39:07 -0600

    add

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,5 @@
+// let nLastModif = Date.parse(document.lastModified);
+console.log(document.lastModified)
+
+// document.getElementById('lastModified')
+document.querySelector('#lastModified').innerHTML = document.lastModified;
---
+++ scripts/getDates.js
@@ -0,0 +1,32 @@
+// Store the selected elements that we are going to use. 
+const navElement = document.querySelector('.active')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	navElement.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+// let nLastModif = Date.parse(document.lastModified);
+console.log(document.lastModified)
+
+// document.getElementById('lastModified')
+document.querySelector('#lastModified').innerHTML = document.lastModified;
+
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
