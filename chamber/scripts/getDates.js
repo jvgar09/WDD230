f4c000c49adacafa58d5c370fbbb18d82d112afe
jvgar10 <maddogjig@hotmail.com>
2024-01-19 15:50:37 -0600

    fix

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -3,18 +3,4 @@ const navElement = document.querySelector('#nav');
 const hambutton = document.querySelector('#menu');
 
 
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambutton.addEventListener('click', () => {
-	navElement.classList.toggle('hide');
-	hambutton.classList.toggle('show');
-});
-
-
-// let nLastModif = Date.parse(document.lastModified);
-console.log(document.lastModified)
-
-// document.getElementById('lastModified')
-document.querySelector('#lastModified').innerHTML = document.lastModified;
-
-
-
+// Add a click event listender to the hamburge
