5b1640357c1652499ce8a128f4bc833b0fa8b17b
jvgar10 <maddogjig@hotmail.com>
2024-01-29 12:29:30 -0600

    add .js

---
+++ scripts/formvalidation.js
@@ -0,0 +1,18 @@
+
+
+function checkPasswordMatch() {
+    var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("confirmPassword");
+    if (password.value !== confirmPassword.value) {
+        alert("Passwords do not match. Please try again.");
+        password.value = "";
+        confirmPassword.value = "";
+        password.focus();
+    }
+}
+
+function updateRatingValue() {
+    var ratingValue = document.getElementById("ratingValue");
+    var pageRating = document.getElementById("pageRating");
+    ratingValue.innerHTML = pageRating.value;
+}
