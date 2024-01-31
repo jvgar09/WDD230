f661c015cb89da3ade1dff8420476126716937e7
jvgar10 <maddogjig@hotmail.com>
2024-01-31 14:12:32 -0600

    add

---
+++ scripts/date.js
@@ -0,0 +1,11 @@
+
+function setTimestamp() {
+    // Get the current date and time
+    const currentTimestamp = new Date();
+
+    // Format the timestamp as per your requirement
+    const formattedTimestamp = `${currentTimestamp.toLocaleDateString()} ${currentTimestamp.toLocaleTimeString()}`;
+
+    // Set the timestamp value to the HTML element with id "timestamp"
+    document.getElementById("timestamp").value = formattedTimestamp;
+}
