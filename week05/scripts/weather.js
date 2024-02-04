cbe7a657f3d94fde9fd06b8325cd8fe68666c714
jvgar10 <maddogjig@hotmail.com>
2024-02-04 01:10:50 -0600

    add

---
+++ week05/scripts/weather.js
@@ -0,0 +1,46 @@
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+
+
+const latitude = 49.7596;
+const longitude = 6.6419;
+
+
+const queryParam = `?lat=${latitude}&lon=${longitude}&units=imperial&appid=66f5836c76e8f5ea0dae08aa7834ee0c`;
+
+
+const fullUrl = `${url}${queryParam}`;
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(fullUrl);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+apiFetch();
+
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
