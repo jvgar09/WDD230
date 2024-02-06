cf85ee5dbfc5f887170e693ee0a64923bdf42f32
jvgar10 <maddogjig@hotmail.com>
2024-02-05 19:06:36 -0600

    add

--- scripts/links.js
+++ scripts/links.js
@@ -22,8 +22,8 @@ const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather';
 
 
-const latitude = 29.65;
-const longitude = -95.28;
+const latitude = 29.80;
+const longitude = -95.61;
 
 
 const queryParam = `?lat=${latitude}&lon=${longitude}&units=imperial&appid=66f5836c76e8f5ea0dae08aa7834ee0c`;
