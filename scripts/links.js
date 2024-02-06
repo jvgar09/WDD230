a528370a41391d0077ec2cf5258f7a4472016302
jvgar10 <maddogjig@hotmail.com>
2024-02-05 19:24:43 -0600

    add

--- scripts/links.js
+++ scripts/links.js
@@ -1,15 +1,15 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
+//const baseURL = "https://yourgithubusername.github.io/wdd230/";
 
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
-
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
+//const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+//
+//async function getLinks() {
+//    const response = await fetch(linksURL);
+//    const data = await response.json();
     //console.log(data);//
-    displayLinks(data);
-  }
+//    displayLinks(data);
+//  }
   
-  getLinks();
+//  getLinks();
 
 
 
@@ -19,17 +19,17 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 
-const url = 'https://api.openweathermap.org/data/2.5/weather';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=29.90&lon=-95.61&appid=66f5836c76e8f5ea0dae08aa7834ee0c';
 
 
-const latitude = 29.80;
-const longitude = -95.61;
+//const latitude = 29.80;
+//const longitude = -95.61;
 
 
-const queryParam = `?lat=${latitude}&lon=${longitude}&units=imperial&appid=66f5836c76e8f5ea0dae08aa7834ee0c`;
+//const queryParam = `?lat=${latitude}&lon=${longitude}&units=imperial&appid=66f5836c76e8f5ea0dae08aa7834ee0c`;
 
 
-const fullUrl = `${url}${queryParam}`;
+//const fullUrl = `${url}${queryParam}`;
 
 
 async function apiFetch() {
