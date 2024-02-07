ac904c910ed310a5ffc9dc6226ea7ad8c1a0fd3f
jvgar10 <maddogjig@hotmail.com>
2024-02-07 14:40:26 -0600

    fix

--- scripts/links.js
+++ scripts/links.js
@@ -1,15 +1,15 @@
-//const baseURL = "https://yourgithubusername.github.io/wdd230/";
+const baseURL = "https://yourgithubusername.github.io/wdd230/";
 
-//const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
-//
-//async function getLinks() {
-//    const response = await fetch(linksURL);
-//    const data = await response.json();
+const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
     //console.log(data);//
-//    displayLinks(data);
-//  }
+    displayLinks(data);
+  }
   
-//  getLinks();
+getLinks();
 
 
 
