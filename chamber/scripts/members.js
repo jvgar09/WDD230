162a48bd245b2ce93381075ca15440f69d0fd843
jvgar10 <maddogjig@hotmail.com>
2024-02-07 12:25:36 -0600

    add

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-   
+
     fetch('data/members.json')
         .then(response => response.json())
         .then(data => displayMembers(data.members));
