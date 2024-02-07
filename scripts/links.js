b574720e9965d40481e191929d897ed0ca0e4dbf
jvgar10 <maddogjig@hotmail.com>
2024-02-07 15:11:00 -0600

    add

--- scripts/links.js
+++ scripts/links.js
@@ -9,11 +9,11 @@ const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);//
     displayLinks(data);
   }
   
-getLinks();
+getLinks(weeks);
 
 
 
