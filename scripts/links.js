0f6cb046a73ed7a68297df92799c173263891444
jvgar10 <maddogjig@hotmail.com>
2024-02-07 18:06:16 -0600

    add

--- scripts/links.js
+++ scripts/links.js
@@ -58,10 +58,4 @@ apiFetch();
 function displayResults(data) {
     currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
     const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconSrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc}`;
-}
-
-
+    let desc = data.weather[0].d
