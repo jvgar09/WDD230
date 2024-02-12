31a19bec35c66a2bdc8ed92ec37de1b49113b45f
jvgar10 <maddogjig@hotmail.com>
2024-02-12 16:27:14 -0600

    add

--- scripts/links.js
+++ scripts/links.js
@@ -1,17 +1,61 @@
-const baseURL = "https://jvgar09.github.io/wdd230/";
-const linksURL = "https://jvgar09.github.io/wdd230/data/links.json";
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
-    displayLinks(data);
+let baseURL = "https://jvgar09.github.io/wdd230/";
+let linksURL = "data/links.json"
+
+const ul = document.getElementById('weekLinks');
+
+
+const displayLinks = (weeks)=> {
+    weeks.forEach((weeks) => {
+        
+       
+        const li = document.createElement('li');
+
+    
+        li.innerHTML = `${weeks.week}: ${weeks.links.map(link => `<a href="${link.url}">${link.title}</a>`).join(' | ')}`;
+
+    
+        ul.appendChild(li);
+        
+    });
+
+ 
 }
-getLinks();
 
-function displayLinks(weeks){
 
+
+
+const getLinks = async()=> {
+    try{
+        const response = await fetch(baseURL + linksURL);
+        if(response.ok)
+        {
+            const data = await response.json();
+            //console.log(data);
+            displayLinks(data.weeks);
+        }
+        else
+        {
+            throw Error(await response.text());
+        }
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
 }
+
+getLinks()
+
+
+
+
+
+
+
+
+
 
 
 // Select HTML elements in the document
