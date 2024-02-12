d089183935b452d66e0f5269a762da2b32feef12
jvgar10 <maddogjig@hotmail.com>
2024-02-12 16:27:41 -0600

    add

--- scripts/links.js
+++ scripts/links.js
@@ -5,43 +5,40 @@ let linksURL = "data/links.json"
 const ul = document.getElementById('weekLinks');
 
 
-const displayLinks = (weeks)=> {
+const displayLinks = (weeks) => {
     weeks.forEach((weeks) => {
-        
-       
+
+
         const li = document.createElement('li');
 
-    
+
         li.innerHTML = `${weeks.week}: ${weeks.links.map(link => `<a href="${link.url}">${link.title}</a>`).join(' | ')}`;
 
-    
+
         ul.appendChild(li);
-        
+
     });
 
- 
+
 }
 
 
 
 
-const getLinks = async()=> {
-    try{
+const getLinks = async () => {
+    try {
         const response = await fetch(baseURL + linksURL);
-        if(response.ok)
-        {
+        if (response.ok) {
             const data = await response.json();
             //console.log(data);
             displayLinks(data.weeks);
         }
-        else
-        {
+        else {
             throw Error(await response.text());
         }
 
     }
-    catch(error)
-    {
+    catch (error) {
         console.log(error);
     }
 }
