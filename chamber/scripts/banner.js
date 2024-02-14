1c8361c138c3c4192a785e598141cdc044e1790a
jvgar10 <maddogjig@hotmail.com>
2024-02-14 17:07:53 -0600

    add

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,7 +1,4 @@
 
-//Script to show banner only on mondays, tuesdays, wednesdays
-//and closing the banner with a button
-
 const closeBtn = document.getElementById("close");
 const displayBanner = document.querySelector(".banner");
 
@@ -13,17 +10,12 @@ function hideBanner(){
 
 
 
-
-// Get the current date
 const currenDate = new Date();
 
-// Get the day of the week as a number (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
 const dayOfWeek = currenDate.getDay();
 
-// Array to map day numbers to day names
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// Get the day name corresponding to the day number
 const currentDayOfWeek = daysOfWeek[dayOfWeek];
 
 if(currentDayOfWeek === 'Monday'){
@@ -40,6 +32,6 @@ else if(currentDayOfWeek === 'Wednesday'){
 }
 else{
     displayBanner.style.display = "none";
-    //console.log(`No today is ${currentDayOfWeek}`);
+   
 
 }
